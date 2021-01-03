dd0e65f5f8d7c3c42271e6bd6c15defc3e313558
Dragos Tanta <dragostanta@Dragoss-MacBook-Pro.local>
2021-01-03 23:52:01 +0200

    added test file

--- client/src/components/utils/ImageSlider.js
+++ client/src/components/utils/ImageSlider.js
@@ -9,7 +9,7 @@ function ImageSlider(props) {
                 {props.images.map((image, index) => (
                     <div key={index}>
                         <img style={{ width: '100%', height: '250px' }}
-                            src={`http://localhost:5000/${image}`} alt="productImage" />
+                            src={`/${image}`} alt="productImage" />
                     </div>
                 ))}
             </Carousel>
