99711297f517adcd353106f425fdee49939e8af5
Sogardel <dragos.tanta@gmail.com>
2021-04-03 08:29:49 +0300

    Update CartPage.js

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -87,7 +87,7 @@ function CartPage(props) {
 
                 <UserCardBlock
                     products={props.user.cartDetail}
-                    removeItem={removeFromCart}//{removeFromCart}
+                    removeItem={transactionSuccess}//{removeFromCart}
                 />
 
 
